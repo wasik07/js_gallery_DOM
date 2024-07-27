125a58aa2952ce08e9a24b68bb1463fb0562bfff
Vasyl Chukhrai <wasik1995@mail.com>
2024-07-27 18:14:14 +0300

    make build

---
+++ index.7f542d39.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector(".gallery__large-img");document.querySelectorAll(".list-item__link").forEach(function(e){return e.onclick=function(e){return e.preventDefault()}}),e.onclick=function(e){return t.src=e.target.closest(".list-item__link").href};
+//# sourceMappingURL=index.7f542d39.js.map
